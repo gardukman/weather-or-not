48fdb9947ff99288696b2595c7e08cfe0fb7c4f8
Duke Nukem <garrettduke87@gmail.com>
2020-09-06 20:05:35 -0600

    broken page

--- assets/styling/weather.js
+++ assets/styling/weather.js
@@ -5,7 +5,7 @@ function myFunction() {
     var apiKey = "a2e4e1f56526a00db1602c368d5b0a05"
 
     // current weather api
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=" + apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=a2e4e1f56526a00db1602c368d5b0a05")
         // {
         //     "method": "GET",
         //     "headers": {}
@@ -20,14 +20,22 @@ function myFunction() {
             var formSubmitHandler = function (event) {
                 event.preventDefault();
 
-                var user
-            }
+                var city = searchTerm.value.trim();
+
+                if(city) {
+                    myFunction(city);
+                    searchTerm.value = "";
+                } else {
+                    alert("Please enter a City.");
+                }
+            };
+
+            searchTerm.addEventListener("submit", formSubmitHandler);
 
             var cityTerm = data.name;
             var cityTemp = data.main.temp;
             var cityHumid = data.main.humidity;
             var cityWind = data.wind.speed;
-            // var cityUV = data.
             var cityLat = data.coord.lat;
             var cityLon = data.coord.lon;
 
@@ -36,7 +44,13 @@ function myFunction() {
             placeCityNameEl.innerHTML = '<h2>' + cityTerm + '</h2>';
 
             const placeCityTempEl = document.getElementById('tempText');
-            placeCityTempEl.textContent = 'Temperature' + cityTemp;
+            placeCityTempEl.textContent = 'Temperature: ' + cityTemp;
+
+            const placeCityHumidEl = document.getElementById('humid-text');
+            placeCityHumidEl.textContent = 'Humidity: ' + cityHumid;
+
+            const placeCityWindEl = document.getElementById('wind-text');
+            placeCityWindEl.textContent = 'Wind Speed: ' + cityWind;
 
             // return fetch for UV index
             return fetch(
@@ -45,6 +59,13 @@ function myFunction() {
             .then(function (uvResponse) {
                 return uvResponse.jason
             })
+            .then(function (data) {
+                console.log(data);
+            
+                var cityUV = data.value
+
+                const placeCityUvEl = document.getElementById('uv-text');
+                placeCityUvEl.textContent = 'UV Index: ' + cityUV
 
 
             // var placeMainCardIconEl
@@ -56,4 +77,6 @@ function myFunction() {
         .catch(err => {
             console.error(err);
         });
-}
+})
+
+};
